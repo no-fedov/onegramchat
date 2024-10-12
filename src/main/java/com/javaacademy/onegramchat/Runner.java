dbf445a5cae6865be83f533f24bd36e0414ff250
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-10-12 17:14:04 +0300

    init commit

---
+++ src/main/java/com/javaacademy/onegramchat/Runner.java
@@ -0,0 +1,8 @@
+package com.javaacademy.onegramchat;
+
+public class Runner {
+    public static void main(String[] args) {
+        // TODO: здесь будем запускать чат
+        System.out.println("Hello World");
+    }
+}
