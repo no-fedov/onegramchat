fdcc1cf3a309beeda232ca82ddecf848411f3a14
Alexandwere <[lazukov.ao@gmail.com]>
2024-10-13 15:22:47 +0300

    fix: Исправил вторые замечания

--- src/main/java/com/javaacademy/onegramchat/model/Message.java
+++ src/main/java/com/javaacademy/onegramchat/model/Message.java
@@ -3,16 +3,22 @@ package com.javaacademy.onegramchat.model;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@Builder
 @Data
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
 public class Message {
     @NonNull
     String text;
+
     @EqualsAndHashCode.Exclude
     boolean isIncome;
+
+    @NonNull
     @ToString.Exclude
     User sender;
+
+    @NonNull
     @ToString.Exclude
     User recipient;
 }
