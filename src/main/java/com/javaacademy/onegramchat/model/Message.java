8c62e9722fe4d774f4d5313b0ba7d6f32452ee04
Alexandwere <lazukov_ao@mail.ru>
2024-10-13 15:49:39 +0300

    feat: добавить сущности "Message" и "User" (#1)

---
+++ src/main/java/com/javaacademy/onegramchat/model/Message.java
@@ -0,0 +1,23 @@
+package com.javaacademy.onegramchat.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Message {
+    @NonNull
+    String text;
+
+    @EqualsAndHashCode.Exclude
+    boolean isIncome;
+
+    @NonNull
+    @ToString.Exclude
+    User sender;
+
+    @NonNull
+    @ToString.Exclude
+    User recipient;
+}
---
+++ src/main/java/com/javaacademy/onegramchat/model/User.java
@@ -0,0 +1,28 @@
+package com.javaacademy.onegramchat.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @NonNull
+    @EqualsAndHashCode.Include
+    String name;
+
+    @NonNull
+    String password;
+
+    @NonNull
+    @Builder.Default
+    List<Message> sentMessages = new ArrayList<>();
+
+    @NonNull
+    @Builder.Default
+    List<Message> receivedMessages = new ArrayList<>();
+}
