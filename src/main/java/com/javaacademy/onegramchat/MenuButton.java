75fc5154a065183b44475b278c4d28b4614db476
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-10-14 16:26:13 +0300

    feat: добавлены кнопки меню

---
+++ src/main/java/com/javaacademy/onegramchat/MenuButton.java
@@ -0,0 +1,20 @@
+package com.javaacademy.onegramchat;
+
+public enum MenuButton {
+    LOGIN(1),
+    REGISTRATION(2),
+    LOGOUT(3),
+    WRITE(4),
+    READ(5),
+    EXIT(6);
+
+    private final int number;
+
+    MenuButton(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
