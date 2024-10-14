c92cea491669570db497b7708258aba791db2161
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-10-14 18:40:27 +0300

    feat: добавлено меню чата

---
+++ src/main/java/com/javaacademy/onegramchat/ChatMenu.java
@@ -0,0 +1,47 @@
+package com.javaacademy.onegramchat;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+@RequiredArgsConstructor
+public class ChatMenu {
+    @NonNull
+    private Chat chat;
+
+    @NonNull
+    private Scanner scanner;
+
+    public void startChat() {
+        while (!chat.isDisable()) {
+            try {
+                // TODO: сделать печать меню в отдельном классе
+                System.out.println("Печатается меню");
+                int userAnswer = scanner.nextInt();
+                Arrays.stream(MenuButton.values())
+                        .filter(button -> userAnswer == button.getNumber())
+                        .findFirst()
+                        .ifPresentOrElse(this::menuAction,
+                                () -> System.out.println("Неверный ввод"));
+            } catch (Exception e) {
+                System.out.println("Сбои программы");
+            }
+        }
+    }
+
+    private void menuAction(MenuButton button) {
+        switch (button) {
+            case LOGIN -> chat.login();
+            case REGISTRATION -> chat.registration();
+            case LOGOUT -> chat.logout();
+            case WRITE -> chat.sendMessage();
+            case READ -> chat.readMessage();
+            case EXIT -> {
+                chat.exit();
+                scanner.close();
+            }
+        }
+    }
+}
