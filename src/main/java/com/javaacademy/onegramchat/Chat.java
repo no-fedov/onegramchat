168c7199185c664ea680b96de66b55c6903feffb
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-10-14 18:22:46 +0300

    feat: разработан абстрактный класс Chat

---
+++ src/main/java/com/javaacademy/onegramchat/Chat.java
@@ -0,0 +1,30 @@
+package com.javaacademy.onegramchat;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Scanner;
+
+@RequiredArgsConstructor
+public abstract class Chat {
+    @Getter
+    private boolean isDisable;
+
+    @NonNull
+    private final Scanner scanner;
+
+    public abstract void login();
+
+    public abstract void registration();
+
+    public abstract void logout();
+
+    public abstract void sendMessage();
+
+    public abstract void readMessage();
+
+    public final void exit() {
+        isDisable = true;
+    }
+}
