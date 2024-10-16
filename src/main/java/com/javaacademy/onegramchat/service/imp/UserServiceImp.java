33ca07845405978ecb3d5b93566779eb3ada9532
no-fedov <145092682+no-fedov@users.noreply.github.com>
2024-10-16 12:23:34 +0300

    perf: улучшить производительность при работе с пользователями (#5)

--- src/main/java/com/javaacademy/onegramchat/service/imp/UserServiceImp.java
+++ src/main/java/com/javaacademy/onegramchat/service/imp/UserServiceImp.java
@@ -3,25 +3,23 @@ package com.javaacademy.onegramchat.service.imp;
 import com.javaacademy.onegramchat.model.User;
 import com.javaacademy.onegramchat.service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static com.javaacademy.onegramchat.util.UserServiceUtil.checkDuplicateUserName;
 
 public class UserServiceImp implements UserService {
-    private final List<User> users = new ArrayList<>();
+    private final Map<String,User> users = new HashMap<>();
 
     @Override
     public void registerUser(User user) {
         checkDuplicateUserName(user.getName(), this);
-        users.add(user);
+        users.put(user.getName(), user);
     }
 
     @Override
     public Optional<User> findUserByName(String name) {
-        return users.stream()
-                .filter(user -> user.getName().equals(name))
-                .findFirst();
+        return Optional.ofNullable(users.get(name));
     }
 }
